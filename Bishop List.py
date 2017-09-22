fc54e9d5cd40f2097f8a086b08f56e2416d4fc82
Joshua Schappel <jmschappel12@gmail.com>
2017-09-22 15:54:31 -0400

    Everything working
    TODO: Code cleanup

--- Bishop List.py
+++ Bishop List.py
@@ -1,8 +1,11 @@
+# Created by Joshua Schappel
+# Built for Renew International
+
 from bs4 import BeautifulSoup
 import requests
 import csv
 
-# Array with all the states and places that are concidered in the US
+# Array with all the states and places that are in the US
 states = [' Alabama',' Alaska',' Arizona',' Arkansas',' California',' Colorado',
          ' Connecticut',' Delaware',' Florida',' Georgia',' Hawaii',' Idaho', 
          ' Illinois',' Indiana',' Iowa',' Kansas',' Kentucky',' Louisiana',
@@ -17,65 +20,73 @@ states = [' Alabama',' Alaska',' Arizona',' Arkansas',' California',' Colorado',
          ]
 
 # List for the data to be stored in
-myList = list()
 list2 = list()
 nameList = list()
 extensionList = list()
 titleLocationList = list()
 
 
-# Get the page
-page = requests.get("http://www.catholic-hierarchy.org/country/bus2.html")
-soup = BeautifulSoup(page.content, 'lxml')
-
-# Since there are many ul tags we will find the child of the tag we want and then go to its parent
-ul = soup.find('br').parent.find_all('li')
-
-
 i = 0
-for li in ul:
-    
-    data =(li.get_text(strip=False))
-    data = data.split(",")
+while i < 3:
 
-    # Data cleanup
-    
-    if "\n\n\n" in data[-1]:
-        data[-1] = data[-1].replace("\n\n\n", "")
-    elif "\r\n" in data[-1]:
-        data[-1] = data[-1].replace("\r\n", "")
-    elif "\n" in data[-1]:
-        data[-1] = data[-1].replace("\n", "")
-    elif "\r" in data[-1]:
-        data[-1] = data[-1].replace("\r","")
+    # Get the page
+    if i == 0:
+        page = requests.get("http://www.catholic-hierarchy.org/country/bus2.html")
+        soup = BeautifulSoup(page.content, 'lxml')
+    elif i == 1:
+        page = requests.get("http://www.catholic-hierarchy.org/country/bus2b.html")
+        soup = BeautifulSoup(page.content, 'lxml')
     else:
-        break  
+        page = requests.get("http://www.catholic-hierarchy.org/country/bus2c.html")
+        soup = BeautifulSoup(page.content, 'lxml')
 
-    # Get the state
-    # This will be our main sorter since it will remove any places that are not in the USA
-    if any(word in data[-1] for word in states):
-        myList.extend(data)
+    # Since there are many ul tags we will find the child of the tag we want and then go to its parent
+    ul = soup.find('br').parent.find_all('li')
+    i = i + 1
+    
+    #Loop that will find and sort the data
+    for li in ul:
+        
+        data =(li.get_text(strip=False))
+        data = data.split(",")
 
-        if len(data) > 2: #If it is 2 or less state is not provided
-            list2.extend([data[-1]])
+        # Data cleanup     
+        if "\n\n\n" in data[-1]:
+            data[-1] = data[-1].replace("\n\n\n", "")
+        elif "\r\n" in data[-1]:
+            data[-1] = data[-1].replace("\r\n", "")
+        elif "\n" in data[-1]:
+            data[-1] = da
+            ta[-1].replace("\n", "")
+        elif "\r" in data[-1]:
+            data[-1] = data[-1].replace("\r","")
+        else:
+            break  
 
-        # Special cases
-        if len(data) == 4:
-            extensionList.extend([data[1]])
-            titleLocationList.extend([data[2]])
-        elif len(data) == 3: # This is the normal case (no extension)
-            titleLocationList.extend([data[1]])
-            extensionList.extend(" ")
-        elif len(data) == 5: # Not sure yet if this case is possiable
-            print("found you")
-        else: # Must be the len = 2 case, Mabye make this the 3 case?
-            extensionList.extend(" ")
-            list2.extend(" ")
-            titleLocationList.extend([data[1]])
-            
-        #Get full name and title
-        nameList.extend([data[0]])
+        # Get the state
+        # This will be our main sorter since it will remove any places that are not in the USA
+        if any(word in data[-1] for word in states):
 
+            if len(data) > 2: #If it is 2 or less state is not provided
+                list2.extend([data[-1]])
+
+            # Special cases
+            if len(data) == 4:
+                extensionList.extend([data[1]])
+                titleLocationList.extend([data[2]])
+            elif len(data) == 3: # This is the normal case (no extension)
+                titleLocationList.extend([data[1]])
+                extensionList.extend(" ")
+            elif len(data) == 5: # Not sure yet if this case is possiable
+                print("found you")
+            else: # Must be the len = 2 case, Mabye make this the 3 case?
+                extensionList.extend(" ")
+                list2.extend(" ")
+                titleLocationList.extend([data[1]])
+                
+            #Get full name and title
+            nameList.extend([data[0]])
+print (list2)
 # Zip the list up into a tuple
 zipList = zip(nameList,titleLocationList,extensionList,list2)
 
@@ -84,4 +95,4 @@ with open('BishopList.csv', 'a', newline='') as csv_file:
     writer = csv.writer(csv_file)
     for row in zipList:
         writer.writerow(row)
-print("all done")
+print("The program is now finished. Please open excel file titled: Bishop List")
